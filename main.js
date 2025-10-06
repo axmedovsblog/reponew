3bfb962e55b5b6819fdc7392f808c5bb534ceb66
feruzbek355 <starkbek455@gmail.com>
2025-10-06 18:44:01 +0500

    Update border radius of h1 element to 10px

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ let h1 = document.querySelector('h1');
 h1.style.color = 'blue';
 h1.style.backgroundColor = 'lightgray';
 h1.style.padding = '10px';
-h1.style.borderRadius = '5px';
+h1.style.borderRadius = '10px';
